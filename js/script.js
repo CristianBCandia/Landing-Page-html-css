6dba4b90bc8b3632870ce03498cc73c61a2f9485
Cristian Bittencourt <cristian.candia7@gmail.com>
2020-08-27 03:18:05 -0300

    CSS: footer implementation and top menu design change

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 let cardContainer = document.querySelector('.card-container')
+let card1 = document.querySelector('.adv1')
+let icons = document.querySelector('.advantages')
 
+window.onscroll = scroll;
+
+function scroll () {
+ //alert("evento scroll detectado! " + window.pageXOffset + " " + window.pageYOffset);
+ 
+}
